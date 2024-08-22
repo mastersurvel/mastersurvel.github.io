c008f858f94bdd73381eb11048630d38ea28a340
mastersurvel <mastersurvel@gmail.com>
2024-08-22 19:29:04 +0700

    first commit

---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int nilai1,nilai2,hasil;
+    nilai1=3;
+    nilai2=6;
+    hasil=nilai1+nilai2;
+
+    cout<<"hasil penjumlahan :"<<hasil;
+
+    cout<<"nilai1:";
+    cin>>nilai1;
+
+    cout<<"nilai 1 ="<<nilai1;
+  
+}
